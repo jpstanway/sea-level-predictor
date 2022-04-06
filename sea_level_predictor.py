a1dc6fd9791f6bab64f5c8de7a815a3179ce5604
Jordan S <jpstanway@gmail.com>
2022-04-06 15:00:56 -0700

    create scatter and line of best fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -5,10 +5,24 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], c='b')
 
     # Create first line of best fit
+    slope, intercept, rvalue, pvalue, stderr = linregress(
+        df['Year'], df['CSIRO Adjusted Sea Level'])
+
+    xline = list(range(1860, 2051))
+
+    yline = []
+
+    for xl in xline:
+        yt = (slope * xl) + intercept
+        yline.append(yt)
+
+    plt.plot(xline, yline, 'r')
 
     # Create second line of best fit
 
